714c1c38bb6a0c597faef087806f5e0a7b7159cb
Simon Kesteloot <simon@kesteloot.me>
2014-02-10 18:08:01 +0100

    correct indentation

--- src/fr/univtours/polytech/di/multimedia/signextractors/test/TestWordExtractor.java
+++ src/fr/univtours/polytech/di/multimedia/signextractors/test/TestWordExtractor.java
@@ -12,19 +12,19 @@ public class TestWordExtractor {
     
     @Before
     public void setUp() throws Exception {
-	WE = new WordExtractor();
+    	WE = new WordExtractor();
     }
 
     @Test
     public void test_WordExtractor() {
-	String str = "Ceci est un test de 39dé.";
-	WE.setContent(str);
-	Assert.assertEquals("0", "Ceci", WE.nextToken());
-	Assert.assertEquals("1", "est", WE.nextToken());
-	Assert.assertEquals("2", "un", WE.nextToken());
-	Assert.assertEquals("3", "test", WE.nextToken());
-	Assert.assertEquals("4", "de", WE.nextToken());
-	Assert.assertEquals("5", "39dé", WE.nextToken());
-	Assert.assertEquals("6", null, WE.nextToken());
+		String str = "Ceci est un test de 39dé.";
+		WE.setContent(str);
+		Assert.assertEquals("0", "Ceci", WE.nextToken());
+		Assert.assertEquals("1", "est", WE.nextToken());
+		Assert.assertEquals("2", "un", WE.nextToken());
+		Assert.assertEquals("3", "test", WE.nextToken());
+		Assert.assertEquals("4", "de", WE.nextToken());
+		Assert.assertEquals("5", "39dé", WE.nextToken());
+		Assert.assertEquals("6", null, WE.nextToken());
     }
 }
