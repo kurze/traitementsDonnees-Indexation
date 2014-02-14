3afa9a8a55005b68798539fb8193a2497782a99a
Erwann Cloarec <erwann.cloarec@gmail.com>
2014-02-14 23:28:02 +0100

    Ajout du modèle vectoriel avec gestion de tf-idf

--- src/fr/univtours/polytech/di/multimedia/querymodels/VectorQueryModel.java
+++ src/fr/univtours/polytech/di/multimedia/querymodels/VectorQueryModel.java
@@ -1,10 +1,15 @@
 package fr.univtours.polytech.di.multimedia.querymodels;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import fr.univtours.polytech.di.multimedia.database.Database;
+import fr.univtours.polytech.di.multimedia.database.Document;
+import fr.univtours.polytech.di.multimedia.database.InvertedIndex;
 import fr.univtours.polytech.di.multimedia.database.ValuedObject;
+import fr.univtours.polytech.di.multimedia.signextractors.WordExtractor;
 
 /**
  * Classe implémentant le modèle d'interrogation vectoriel.
@@ -31,9 +36,57 @@ public class VectorQueryModel extends QueryModel {
   @Override
   public List < ValuedObject > getAnswers(final String question) {
     final List < ValuedObject > results = new ArrayList < ValuedObject >();
+       
+    int nbTotalDocs = getDatabase().getDocuments().size();
+    
+    //extractions des mots de la question
+    ArrayList<String> questionWords = new ArrayList<String>();
+    WordExtractor questionWordExtractor = new WordExtractor();
+    questionWordExtractor.setContent(question);
+    String word;
+    while((word = questionWordExtractor.nextToken()) != null){
+      questionWords.add(word);
+    }
+    
+    InvertedIndex invertedIndex = getDatabase().getInvertedIndex();
+    List<Document> docs = new ArrayList<Document>();
+    for (String questionWord : questionWords) {
+	docs.addAll(invertedIndex.getAllDocuments(questionWord));
+    }
+    
+    for (Document document : docs) {
+	double somme = 0;
+	double sommeCarre = 0;
+	double sommeWord = 0;
+	for (String questionWord : questionWords) {
+	    double occurence = invertedIndex.getWordOccurrences(questionWord, document);
+	    if(useTFIDF) {
+		int nbDocs = invertedIndex.getAllDocuments(questionWord).size();
+		occurence = occurence * (nbTotalDocs / nbDocs); //TODO maybe we can use log() or log(1+) or even ln
+	    }
+	    somme += occurence;
+	    sommeCarre += (occurence * occurence);
+	    sommeWord++;
+	}
+	if(somme != 0) {
+	    double normes = Math.sqrt(sommeCarre) * Math.sqrt(sommeWord);
+	    double cos = (normes==0?0:somme/normes);
+	    results.add(new ValuedObject(document, cos));
+	}
+    }
+    
+    Collections.sort(results, new Comparator<ValuedObject>() {
 
-    // TODO : A COMPLETER ICI
-
+	@Override
+	public int compare(ValuedObject o1, ValuedObject o2) {
+	    if(o1.getScore() < o2.getScore())
+		return 1;
+	    if(o1.getScore() > o2.getScore())
+		return -1;
+	    return 0;
+	}
+    
+    });
     return results;
   }
 }
