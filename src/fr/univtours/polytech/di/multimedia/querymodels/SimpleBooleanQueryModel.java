5c41ebfa66214aa803a12047f0b5c143aabe2077
Erwann Cloarec <erwann.cloarec@gmail.com>
2014-02-15 17:19:42 +0100

    Ajout du modèle booléen

--- src/fr/univtours/polytech/di/multimedia/querymodels/SimpleBooleanQueryModel.java
+++ src/fr/univtours/polytech/di/multimedia/querymodels/SimpleBooleanQueryModel.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.univtours.polytech.di.multimedia.database.Database;
+import fr.univtours.polytech.di.multimedia.database.Document;
+import fr.univtours.polytech.di.multimedia.database.InvertedIndex;
 import fr.univtours.polytech.di.multimedia.database.ValuedObject;
+import fr.univtours.polytech.di.multimedia.signextractors.WordExtractor;
 
 /**
  * Classe implémentant le modèle d'interrogation booléen simple.
@@ -12,13 +15,14 @@ import fr.univtours.polytech.di.multimedia.database.ValuedObject;
  */
 public class SimpleBooleanQueryModel extends QueryModel {
 
+    boolean questionMethodAND = true; //Je ne sais pas si chaque mot de la question sont liés par ET ou OU... 
+    
   /**
    * Le constructeur.
    * @param database la base de données
    */
   public SimpleBooleanQueryModel(final Database database) {
     super(database);
-    // TOTO : A COMPLETER ICI
   }
 
   /**
@@ -29,7 +33,51 @@ public class SimpleBooleanQueryModel extends QueryModel {
   public List < ValuedObject > getAnswers(final String question) {
     final List < ValuedObject > results = new ArrayList < ValuedObject >();
 
-    // TODO : A COMPLETER ICI
+    //extractions des mots de la question
+    ArrayList<String> questionWords = new ArrayList<String>();
+    WordExtractor questionWordExtractor = new WordExtractor();
+    questionWordExtractor.setContent(question);
+    String word;
+    while((word = questionWordExtractor.nextToken()) != null){
+	questionWords.add(word);
+    }
+    
+    InvertedIndex invertedIndex = getDatabase().getInvertedIndex();
+    List<Document> allDocs = new ArrayList<Document>();
+    for (String questionWord : questionWords) {
+	List<Document> docs = invertedIndex.getAllDocuments(questionWord);
+	if(questionMethodAND) {
+	    if(allDocs.size() == 0) {
+		allDocs.addAll(docs);
+	    }
+	    else {
+		List<Document> toDelete = new ArrayList<Document>();
+		for (Document document : allDocs) {
+		    if(!docs.contains(document)) {
+			toDelete.add(document);
+		    }
+		}
+		for (Document document : toDelete) {
+		    allDocs.remove(document);
+		}
+	    }
+	}
+	else {
+	    
+	    for (Document document : docs) {
+		if(!allDocs.contains(document)){
+		    results.add(new ValuedObject(document, 1.0));
+		    allDocs.add(document);
+		}
+	    }
+	}
+    }
+    if(questionMethodAND){
+	for (Document document : allDocs) {
+	    results.add(new ValuedObject(document, 1));
+	}
+    }
+    
     return results;
   }
 
