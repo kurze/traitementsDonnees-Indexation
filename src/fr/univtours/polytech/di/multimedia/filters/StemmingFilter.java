3e47afbe91a7be880f2bc0365790af0be18c56bd
Simon Kesteloot <simon@kesteloot.me>
2014-02-17 16:24:15 +0100

    reindent StemmingFilter

--- src/fr/univtours/polytech/di/multimedia/filters/StemmingFilter.java
+++ src/fr/univtours/polytech/di/multimedia/filters/StemmingFilter.java
@@ -2,45 +2,47 @@ package fr.univtours.polytech.di.multimedia.filters;
 
 /**
  * Classe de filtre réalisant un stemming.
+ * 
  * @author Sébastien Aupetit
  */
 public class StemmingFilter implements Filter {
 
-    
-    String[] suffixes = {
-	    "ance",
-	    "ique",
-	    "isme",
-	    "able",
-	    "iste",
-	    "eux",
-	    "ances",
-	    "iques",
-	    "ismes",
-	    "ables",
-	    "istes",
-	    "atrice",
-	    "ateur",
-	    "ation",
-	    "atrices",
-	    "ateurs",
-	    "ations"
-    };
-  /**
-   * {@inheritDoc}
-   * @see fr.univtours.polytech.di.multimedia.filters.Filter#filter(java.lang.String)
-   */
-  @Override
-  public String filter(final String sign) {
-    String newSign = sign;
-      for (String suffixe : suffixes) {
-	if(newSign.endsWith(suffixe))
-	{
-	    newSign = (String) sign.subSequence(0, sign.length() - suffixe.length());
-	    break;
+	String[] suffixes = {
+			"ance", 
+			"ique", 
+			"isme", 
+			"able", 
+			"iste", 
+			"eux",
+			"ances", 
+			"iques", 
+			"ismes", 
+			"ables", 
+			"istes", 
+			"atrice", 
+			"ateur",
+			"ation", 
+			"atrices", 
+			"ateurs", 
+			"ations" 
+		};
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see fr.univtours.polytech.di.multimedia.filters.Filter#filter(java.lang.String)
+	 */ 
+	@Override
+	public String filter(final String sign) {
+		String newSign = sign;
+		// retrait des suffixes de la base 
+		for (String suffixe : suffixes) {
+			if (newSign.endsWith(suffixe)) { 
+				newSign = (String) sign.subSequence(0, sign.length() - suffixe.length());
+				break;
+			}
+		}
+		return newSign;
 	}
-    }
-    return newSign;
-  }
 
 }
